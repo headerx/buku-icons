70afb84721d81d04f82a03411c90836ae8d9425a
inmanturbo <inman.turbo@gmail.com>
2021-07-06 11:25:49 -0400

    add set method

--- src/Concerns/HasIcon.php
+++ src/Concerns/HasIcon.php
@@ -2,6 +2,7 @@
 
 namespace HeaderX\BukuIcons\Concerns;
 
+use HeaderX\BukuIcons\Actions\FindIconByNameOrIdAction;
 use HeaderX\BukuIcons\Models\Icon;
 
 trait HasIcon
@@ -9,5 +10,10 @@ trait HasIcon
     public function icon(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Icon::class);
+    }
+
+    public function setIconIdAttribute($value)
+    {
+        return (new FindIconByNameOrIdAction)($value)->id;
     }
 }
